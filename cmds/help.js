b7f0b1d25eed3856efdab9561e3e450b10e83e3a
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-01-23 22:00:34 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,29 +3,15 @@ const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 module.exports = {
     name: 'help',
-    description: 'List all available commands with pagination',
-    execute(api, event, args) {
+    description: 'List all available commands',
+    execute(api, event) {
         const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
-        const commandsPerPage = 5; // Nombre de commandes par page
-        
-        let page = parseInt(args[0]) || 1;
-        let totalPages = Math.ceil(commandFiles.length / commandsPerPage);
-        
-        if (page < 1) page = 1;
-        if (page > totalPages) page = totalPages;
-        
-        let message = `C ° O ° M ° M ° A ° N ° D ° S (Page ${page}/${totalPages})\n\n`;
-        
-        const startIndex = (page - 1) * commandsPerPage;
-        const endIndex = startIndex + commandsPerPage;
-        
-        commandFiles.slice(startIndex, endIndex).forEach(file => {
-            const command = require(`./cmds/${file}`);
+        let message = 'C ° O ° M ° M ° A ° N ° D ° S\n\n';
+
+        commandFiles.forEach(file => {
+            const command = require(`./${file}`);
             message += `[~] ${command.name}\n[°°°] ${command.description}\n\n`;
         });
-        
-        message += `Utilisez "!help <numéro de page>" pour naviguer.`;
-        
         api.sendMessage(message, event.threadID);
     }
 };
