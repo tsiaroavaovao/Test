647977e701a70f9ca19ba633efe56fc85347cad1
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-01 14:40:54 +0300

    Update ai.js

--- cmds/ai.js
+++ cmds/ai.js
@@ -3,31 +3,27 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 module.exports = {
-    name: 'kai',
-    description: 'Ask an AI question with Mistral API',
-    async execute(api, event, args) {
-        const question = args.join(' ');
-        const uid = event.senderID; // Utilisation de l'ID de l'utilisateur pour le suivi de conversation
+    name: 'ai',
+    description: 'Ask an AI question with grok-2',
+    async execute(api, event, args) {
+        const question = args.join(' ');
 
-        if (!question) {
-            api.sendMessage(`Veuillez entrer une question.\nUsage: ${config.prefix}ai <votre question>`, event.threadID, event.messageID);
-            return;
-        }
+        if (!question) {
+            api.sendMessage(`Please enter a question.\nUsage: ${config.prefix}ai <your question>`, event.threadID, event.messageID);
+            return;
+        }
 
-        api.sendMessage("Génération en cours...", event.threadID, event.messageID);
+        api.sendMessage("Generating...", event.threadID, event.messageID);
 
-        try {
-            const response = await axios.get(`https://api-mistral-hugging-face.vercel.app/mistralhugging?question=${encodeURIComponent(question)}&uid=${uid}`);
-            
-            if (response.data && response.data.length > 0) {
-                const generatedText = response.data[0].generated_text;
-                api.sendMessage(`🇲🇬BOT MADA🇲🇬\n\n${generatedText}`, event.threadID, event.messageID);
-            } else {
-                api.sendMessage("Erreur lors de la génération de la réponse. Veuillez réessayer plus tard.", event.threadID, event.messageID);
-            }
-        } catch (error) {
-            api.sendMessage("Une erreur est survenue lors du traitement de votre demande. Veuillez réessayer plus tard.", event.threadID, event.messageID);
-        }
-    }
+        try {
+            const response = await axios.get(`https://ajiro.gleeze.com/api/ai?model=grok-2&system=You are a LLM called groq invented by elon musk&question=${encodeURIComponent(question)}`);
+            if (response.data.success) {
+                api.sendMessage(`{}=====GROK-2====={}\n\n` + response.data.response, event.threadID, event.messageID);
+            } else {
+                api.sendMessage("There was an error processing your request. Please try again later.", event.threadID, event.messageID);
+            }
+        } catch (error) {
+            api.sendMessage("There was an error processing your request. Please try again later.", event.threadID, event.messageID);
+        }
+    }
 };
-
